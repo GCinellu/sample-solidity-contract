6ff25dc32265eb278d37a5e71d46b56ee02fb087
Gianluca <gcinellu@gmail.com>
2022-01-16 01:17:36 +0100

    working contract

--- truffle-config.js
+++ truffle-config.js
@@ -41,11 +41,18 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    },
+      development: {
+          host: "127.0.0.1",     // Localhost (default: none)
+          port: 8545,            // Standard Ethereum port (default: none)
+          network_id: "*",       // Any network (default: none)
+      },
+      ropsten: {
+          network_id: "*",
+          host: '127.0.0.1',
+          port: 8545,
+          from: "0x162c80E6b46ae53B3d36f17754D2731757772548"
+      },
+
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
